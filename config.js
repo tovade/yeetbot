9be7859eb665d0f587a97820f43f898eab41e2b9
tovade <66245404+tovade@users.noreply.github.com>
2020-12-05 13:25:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "token": "NzI1MDY5NzQzMDI2ODY0MTU4.XvJX0Q.zItiB1k-bAXt4YOObdf5fBC-ZY4", // https://discordapp.com/developers/applications/ID/bot
-  "mongodbUrl": "mongodb+srv://tovade:coltzIsGood@cluster0.x0vx1.gcp.mongodb.net/yeetbot", // Mongodb connection url.
-  "id": "725069743026864158", // https://discordapp.com/developers/applications/ID/information
-  "clientSecret": "2WrGuMiJdHNtyRleXDJeUSXpg63Tu6Vr", // https://discordapp.com/developers/applications/ID/information
+  "token": "no", // https://discordapp.com/developers/applications/ID/bot
+  "mongodbUrl": "mongo url", // Mongodb connection url.
+  "id": "nope", // https://discordapp.com/developers/applications/ID/information
+  "clientSecret": "nor", // https://discordapp.com/developers/applications/ID/information
   "domain": "http://www.yeetbot.gq",
   "port": 3000
 };
